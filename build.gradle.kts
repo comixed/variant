86b18f70b4bba376f67ec81af041cc5e1a898014
Darryl L. Pierce <mcpierce@gmail.com>
2023-03-19 10:36:06 -0400

    Added a GitHub action for SonarQube build analysis [#1]

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,19 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("7.4.2").apply(false)
     id("com.android.library").version("7.4.2").apply(false)
+    id("org.sonarqube").version("3.4.0.2513")
     kotlin("android").version("1.8.0").apply(false)
     kotlin("multiplatform").version("1.8.0").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "comixed_prestige")
+        property("sonar.organization", "comixed")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
