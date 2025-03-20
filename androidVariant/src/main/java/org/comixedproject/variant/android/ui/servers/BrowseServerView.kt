186957d1cff59ca9b814794a2045b4fc45fdff3b
Darryl L. Pierce <mcpierce@gmail.com>
2025-03-20 14:39:09 -0400

    Added reloading a server's directory on Android [#66]
     * Changed parent link on browsing to the parent's directory.

--- androidVariant/src/main/java/org/comixedproject/variant/android/ui/servers/BrowseServerView.kt
+++ androidVariant/src/main/java/org/comixedproject/variant/android/ui/servers/BrowseServerView.kt
@@ -18,26 +18,25 @@
 
 package org.comixedproject.variant.android.ui.servers
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
-import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
-import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import org.comixedproject.variant.android.R
 import org.comixedproject.variant.android.VariantTheme
@@ -50,87 +49,73 @@ import org.comixedproject.variant.shared.platform.Logger
 
 private val TAG = "BrowseServerView"
 
-@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@OptIn(ExperimentalMaterial3AdaptiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun BrowseServerView(
     server: Server,
+    directory: String,
+    parentDirectory: String?,
+    isLoading: Boolean,
     title: String,
-    parentServerLink: ServerLink?,
     serverLinkList: List<ServerLink>,
     onLoadDirectory: (Server, String, Boolean) -> Unit
 ) {
-    val navigator = rememberListDetailPaneScaffoldNavigator<Any>()
+    val pullToRefreshState = rememberPullToRefreshState()
 
-    BackHandler(navigator.canNavigateBack()) { navigator.navigateBack() }
-
-    NavigableListDetailPaneScaffold(
-        navigator = navigator,
-        listPane = {
-            Column(modifier = Modifier.fillMaxWidth()) {
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    parentServerLink?.let { parentLink ->
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
-                            contentDescription = stringResource(
-                                R.string.navigateBackLabel
-                            ),
-                            modifier = Modifier.clickable {
-                                Logger.d(
-                                    TAG,
-                                    "Going back to parent: ${parentLink.downloadLink}"
-                                )
-                                onLoadDirectory(
-                                    server,
-                                    parentLink.directory,
-                                    false
-                                )
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        title,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                },
+                navigationIcon = {
+                    parentDirectory?.let { parentDirectory ->
+                        IconButton(onClick = {
+                            Logger.d(TAG, "Going back to parent: ${parentDirectory}")
+                            onLoadDirectory(server, parentDirectory, false)
+                        }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                contentDescription = stringResource(R.string.navigateBackLabel)
+                            )
+                        }
+                    }
+                })
+        },
+        content = { padding ->
+            PullToRefreshBox(
+                modifier = Modifier.padding(padding),
+                isRefreshing = isLoading,
+                state = pullToRefreshState,
+                onRefresh = {
+                    Logger.d(TAG, "Reloading directory: ${directory}")
+                    onLoadDirectory(server, directory, true)
+                }, content = {
+                    LazyColumn {
+                        items(
+                            items = serverLinkList.sortedBy { link -> link.title },
+                            itemContent = { serverLink ->
+                                if (serverLink.linkType == ServerLinkType.NAVIGATION) {
+                                    NavigationLinkView(serverLink, onLoadLink = { directory ->
+                                        onLoadDirectory(server, serverLink.downloadLink, false)
+                                    })
+                                } else {
+                                    PublicationLinkView(serverLink, onLoadLink = { directory ->
+                                        onLoadDirectory(server, serverLink.downloadLink, false)
+                                    })
+                                }
                             }
                         )
                     }
-
-                    Text(
-                        title,
-                        fontWeight = FontWeight.Bold,
-                        style = MaterialTheme.typography.headlineMedium
-                    )
                 }
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    items(
-                        items = serverLinkList,
-                        itemContent = { serverLink ->
-                            if (serverLink.linkType == ServerLinkType.NAVIGATION) {
-                                NavigationLinkView(serverLink, onLoadLink = { directory ->
-                                    onLoadDirectory(server, serverLink.downloadLink, false)
-                                })
-                            } else {
-                                PublicationLinkView(serverLink, onLoadLink = { directory ->
-                                    onLoadDirectory(server, serverLink.downloadLink, false)
-                                })
-                            }
-                        }
-                    )
-                }
-            }
-        },
-        detailPane = {
-            Logger.d(TAG, "Showing the detail pane: ${navigator.currentDestination}")
+            )
         }
     )
-}
-
-@Composable
-@Preview
-fun BrowseServerPreview_withParent() {
-    VariantTheme {
-        BrowseServerView(
-            SERVER_LIST.get(0),
-            SERVER_LIST.get(0).name,
-            SERVER_LINK_LIST.get(0),
-            SERVER_LINK_LIST,
-            onLoadDirectory = { _, _, _ -> })
-    }
 }
 
 @Composable
@@ -139,8 +124,25 @@ fun BrowseServerPreview_withoutParent() {
     VariantTheme {
         BrowseServerView(
             SERVER_LIST.get(0),
-            SERVER_LIST.get(0).name,
+            SERVER_LINK_LIST.get(0).directory,
             null,
+            false,
+            SERVER_LIST.get(0).name,
+            SERVER_LINK_LIST,
+            onLoadDirectory = { _, _, _ -> })
+    }
+}
+
+@Composable
+@Preview
+fun BrowseServerPreview_withParent() {
+    VariantTheme {
+        BrowseServerView(
+            SERVER_LIST.get(0),
+            SERVER_LINK_LIST.get(0).directory,
+            SERVER_LINK_LIST.get(0).directory,
+            false,
+            SERVER_LIST.get(0).name,
             SERVER_LINK_LIST,
             onLoadDirectory = { _, _, _ -> })
     }
