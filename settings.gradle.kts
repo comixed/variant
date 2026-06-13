b06d2eb7b252d4dbaa81abacb833db88b2ed3ef9
Darryl L. Pierce <mcpierce@gmail.com>
2026-06-12 20:33:04 -0400

    Added an app icon [#130]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,8 @@ pluginManagement {
     mavenCentral()
   }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-}
+
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0" }
 
 dependencyResolutionManagement {
   repositories {
